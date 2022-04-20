e4344d82f9255d31bccb155cdaed3236c4a1a679
Roberto Tavitas <robertomirandatavitas@gmail.com>
2022-04-20 17:43:35 -0500

    app.js -> events to show or hide mobile navbar, search form, cart 	hide all on scroll
    style.css -> fixed typo

---
+++ app.js
@@ -0,0 +1,33 @@
+// Show/hide navbar
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+// Show/hide search form
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+// Show/hide cart 
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+// 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
